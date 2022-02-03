207d865b562aae0b5b4e27f94f20ec489b2f5b4b
Ad3bay0c <aclintonadebayo@gmail.com>
2022-02-03 12:14:29 +0100

    changed from sql to sqlx

--- application/routes.go
+++ application/routes.go
@@ -1,16 +1,16 @@
 package application
 
 import (
-	"database/sql"
 	"github.com/Ad3bay0c/graphqlTesting/application/handler"
 	"github.com/Ad3bay0c/graphqlTesting/domain/blogRepository"
 	service "github.com/Ad3bay0c/graphqlTesting/service/blogService"
 	"github.com/gin-gonic/gin"
+	"github.com/jmoiron/sqlx"
 	_ "github.com/mattn/go-sqlite3"
 	"log"
 )
 
-func defineRoutes(r *gin.Engine, db *sql.DB) {
+func defineRoutes(r *gin.Engine, db *sqlx.DB) {
 	b := &handler.BlogHandler{Service: service.NewBlogService(blogRepository.NewBlogDB(db))}
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
@@ -22,12 +22,12 @@ func defineRoutes(r *gin.Engine, db *sql.DB) {
 
 func SetupRouter() *gin.Engine {
 	router := gin.New()
-	db, err := sql.Open("sqlite3", "./blog.db")
+	db, err := sqlx.Connect("sqlite3", "./blog.db")
 	if err != nil {
 		log.Fatalf("Error connecting to database: %v", err.Error())
 	}
 	log.Printf("Database Connected Succesfully\n")
-	defer db.Close()
+
 	defineRoutes(router, db)
 
 	return router
