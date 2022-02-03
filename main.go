1f4dd7b4989180b895be85202eef29180744f45a
Ad3bay0c <aclintonadebayo@gmail.com>
2022-02-03 07:22:41 +0100

    start server function

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"github.com/Ad3bay0c/graphqlTesting/application"
+	"log"
+)
 
+func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	application.Start()
 }
