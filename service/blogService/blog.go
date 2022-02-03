ad969cc4e950e1d09e9866c3a5bb04edbaf65795
Ad3bay0c <aclintonadebayo@gmail.com>
2022-02-03 09:21:45 +0100

    blog service implemented

--- service/blogService/blog.go
+++ service/blogService/blog.go
@@ -1,9 +1,13 @@
-package blog
+package blogService
 
-import domain "github.com/Ad3bay0c/graphqlTesting/domain/blog"
+import (
+	"errors"
+	domain "github.com/Ad3bay0c/graphqlTesting/domain/blogRepository"
+	"github.com/graphql-go/graphql"
+)
 
 type Blog interface {
-	List() []domain.Blog
+	Query(query string) (interface{}, error)
 }
 
 type DefaultBlogService struct {
@@ -15,7 +19,32 @@ func NewBlogService(repo domain.BlogRepository) *DefaultBlogService {
 		repo: repo,
 	}
 }
+func (s *DefaultBlogService) rootQuery() *graphql.Object {
+	return graphql.NewObject(graphql.ObjectConfig{
+		Name: "query",
+		Fields: graphql.Fields{
+			"list": &graphql.Field{
+				Type: graphql.NewList(domain.BlogType),
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					return []domain.Blog{{ID: 1, Title: "First result"}, {ID: 2, Title: "Second Result"}}, nil
+				},
+			},
+		},
+	})
+}
+func (s *DefaultBlogService) Query(query string) (interface{}, error) {
+	rootQuery := s.rootQuery()
+	schema, err := graphql.NewSchema(graphql.SchemaConfig{
+		Query: rootQuery,
+	})
+	if err != nil {
+		return nil, errors.New("error creating schema")
+	}
+	params := graphql.Params{
+		Schema:        schema,
+		RequestString: query,
+	}
+	result := graphql.Do(params)
 
-func (s *DefaultBlogService) List() []domain.Blog {
-	return s.repo.GetAll()
+	return result, err
 }
