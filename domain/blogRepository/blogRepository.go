8ce111ee67d1e1fa14ee97dfc8198e64716321a1
Ad3bay0c <aclintonadebayo@gmail.com>
2022-02-03 13:28:04 +0100

    implemented createblog method

--- domain/blogRepository/blogRepository.go
+++ domain/blogRepository/blogRepository.go
@@ -18,8 +18,13 @@ func (db *blogDB) GetAll() ([]Blog, error) {
 	return blog, err
 }
 
-func (db *blogDB) GetByID(id int) (*Blog, error) {
+func (db *blogDB) GetByID(id string) (*Blog, error) {
 	var blog Blog
 	err := db.DB.Get(&blog, "SELECT * FROM blog WHERE id=?", id)
+	return &blog, err
+}
+
+func (db *blogDB) CreateBlog(blog Blog) (*Blog, error) {
+	_, err := db.DB.Exec("INSERT INTO blog(id, title, author) VALUES($1, $2, $3)", blog.ID, blog.Title, blog.Author)
 	return &blog, err
 }
